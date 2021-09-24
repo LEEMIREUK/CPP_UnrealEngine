e1dc02f27ecebf952b1b745a9901e224446c3bf4
Lee Mi Reuk <sslejds@naver.com>
2021-09-24 22:53:02 +0900

    Create C++ class and Apply MeshComponent
    C++ 클래스 생성 이후 MeshComponent 추가
    리소스 로드 후 적용
    인게임에서 사용할 정보 추가

---
+++ TestUnrealEngine/Source/TestUnrealEngine/MyActor.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MESH"));
+	RootComponent = Mesh;
+
+	// 리소스를 로드
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM(TEXT("StaticMesh'/Game/StarterContent/Props/SM_Couch.SM_Couch'"));
+
+	// 만약 로드가 되었다면
+	if (SM.Succeeded())
+	{
+		// Mesh에 저장
+		Mesh->SetStaticMesh(SM.Object);
+	}
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
